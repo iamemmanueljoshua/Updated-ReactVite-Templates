c8a19c34d2b5456ecc693d57ef0458efc189c4fe
Emmanuel Joshua <iamemmanueljoshua@gmail.com>
2023-05-23 14:11:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '', assetsDir: '.',
+	server: {
+		port: 8080
+	},
+	preview: {
+		port: 8080
+	}
 })
